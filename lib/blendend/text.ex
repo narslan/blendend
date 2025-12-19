da8393e66ae90d7b93391f37c24a8cb94d38fe54
Nevroz Arslan <newrozarslan@gmail.com>
2025-12-19 18:47:03 +0300

    fix: Correct shape/2 and metrics/2 return value

--- lib/blendend/text.ex
+++ lib/blendend/text.ex
@@ -30,14 +30,23 @@ defmodule Blendend.Text do
     * returns the resulting `GlyphRun`
 
   Use this when you want to inspect or reuse the shaped glyphs.
-  """
-  @spec shape(font(), String.t()) :: glyph_run()
-  def shape(font, text) do
-    {:ok, gb} = GlyphBuffer.new()
 
-    with :ok <- GlyphBuffer.set_utf8_text(gb, text),
-         :ok <- Font.shape(font, gb) do
-      {:ok, GlyphRun.new(gb)}
+  On success, returns a `GlyphRun`.
+
+  On failure, returns `{:error, reason}`.
+
+  ## Examples
+
+      gr = Blendend.Text.shape(font, "blendend!")
+      Blendend.Text.GlyphRun.fill!(canvas, font, 60, 245, gr, fill: rgb(60, 60, 60))
+  """
+  @spec shape(font(), String.t()) :: glyph_run() | {:error, term()}
+  def shape(font, text) do
+    with {:ok, gb} <- GlyphBuffer.new(),
+         :ok <- GlyphBuffer.set_utf8_text(gb, text),
+         :ok <- Font.shape(font, gb),
+         {:ok, gr} <- GlyphRun.new(gb) do
+      gr
     end
   end
 
@@ -45,13 +54,24 @@ defmodule Blendend.Text do
   Computes text metrics for the given `text` and `font`.
 
   Internally this uses a glyph buffer and `Font.get_text_metrics/2`.
-  .
-  """
-  @spec metrics(font(), String.t()) :: term() | {:error, term()}
-  def metrics(font, text) do
-    gb = GlyphBuffer.new()
 
-    with :ok <- GlyphBuffer.set_utf8_text(gb, text) do
+  ## Examples
+
+       iex>Blendend.Text.metrics(font, "blendend")
+          {:ok,
+             %{
+               "advance_x" => 183.264,
+               "advance_y" => -0.0,
+               "bbox_x0" => 1.104,
+               "bbox_x1" => 183.264,
+               "bbox_y0" => -0.0,
+               "bbox_y1" => -0.0
+              }}
+  """
+  @spec metrics(font(), String.t()) :: {:ok, map()} | {:error, term()}
+  def metrics(font, text) do
+    with {:ok, gb} <- GlyphBuffer.new(),
+         :ok <- GlyphBuffer.set_utf8_text(gb, text) do
       Font.get_text_metrics(font, gb)
     end
   end
